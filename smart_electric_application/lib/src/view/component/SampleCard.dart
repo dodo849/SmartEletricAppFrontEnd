525c6810b3afe75c6e24904c7366973380a97a48
leedoyeon849 <do83430208@gmail.com>
2022-08-02 16:29:22 +0900

    [Feat] 예상 요금 뷰 구현

--- smart_electric_application/lib/src/view/component/SampleCard.dart
+++ smart_electric_application/lib/src/view/component/SampleCard.dart
@@ -9,12 +9,13 @@ class SampleCard extends StatelessWidget {
   Widget build(BuildContext context) {
 
     // 데이터
-    var dataValue = "data";
-    var dataDetailValue = "detail data";
+    var dataValue = "53,120원";
+    var bodyValue = "325 kWh";
+    var bodyDetailValue = "12";
 
     // 박스 내의 텍스트 위젯
     var title = Text(
-            "Sample", 
+            "이번달 예상 요금", 
             style: TextStyle(
               fontSize: 14, 
               fontWeight: FontWeight.bold,
@@ -32,11 +33,20 @@ class SampleCard extends StatelessWidget {
       );   
 
     var chargeDetail = Text(
-        " / ${dataDetailValue}", 
+        " / ${bodyValue}", 
         style: TextStyle(
           fontSize: 14, 
           fontWeight: FontWeight.bold,
           color: context.theme.colorScheme.onSecondary,
+        ),
+      );
+
+      var chargeCompare = Text(
+        " 지난달 대비 ${bodyDetailValue}% ▼", 
+        style: TextStyle(
+          fontSize: 12, 
+          fontWeight: FontWeight.normal,
+          color: context.theme.colorScheme.primaryContainer,
         ),
       );
   
@@ -53,13 +63,25 @@ class SampleCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           title,
+          SizedBox(height: 5),
           Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               charge,
               chargeDetail,
             ],
-          )
+          ),
+          SizedBox(height: 7),
+          chargeCompare,
+          SizedBox(height: 3),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(context.theme.colorScheme.primaryContainer),
+              foregroundColor: MaterialStateProperty.all(context.theme.colorScheme.onSurface),
+              shadowColor: MaterialStateProperty.all(Colors.transparent),
+            ),
+            onPressed: (){}, 
+            child: Text("AI 분석 리포트 보기 > "))
         ],
       )
     );
