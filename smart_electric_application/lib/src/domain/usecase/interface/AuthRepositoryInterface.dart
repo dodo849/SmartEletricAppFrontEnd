e376ad66c6548e0916e52afc0a8a79133aa73607
leedoyeon849 <do83430208@gmail.com>
2022-10-07 11:57:30 +0900

    [Minor] AuthRepoistory 함수 선언 순서 변경

--- smart_electric_application/lib/src/domain/usecase/interface/AuthRepositoryInterface.dart
+++ smart_electric_application/lib/src/domain/usecase/interface/AuthRepositoryInterface.dart
@@ -6,6 +6,7 @@ abstract class AuthRepositoryInterface {
   Future<Result<JwtTokenDTO, String>> getJwtTokens(String firebaseIdToken);
   Future<Result<JwtTokenDTO, String>> readTokens();
   Future<Result<bool, String>> saveJwtTokens(JwtTokenDTO tokens);
+  Future<Result<bool, String>> removeJwtTokens();
   Future<Result<bool, String>> checkEmailDuplicate(String email);
   Future<Result<bool, String>> saveEmail(String email);
   Future<Result<bool, String>> saveUser(
@@ -13,7 +14,6 @@ abstract class AuthRepositoryInterface {
       required String name,
       required String email});
   Future<Result<String, String>> getCustomerNumber();
-  Future<Result<bool, String>> removeJwtTokens();
   Future<Result<bool, String>> removeEmail(email);
   Future<Result<String, String>> getEmail();
   Future<Result<bool, String>> removeUser();
