6e3b0fbb6c1edd6963fb6a53eec93c3c8550d88d
kimjinyeon <hourglass458@gmail.com>
2022-10-25 17:25:43 +0900

    [Feat] AccountEmailValidationDTO 구현 1. 이메일 중복 확인

---
+++ smart_electric_application/lib/src/data/dto/AccountEmailValidationDTO.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'AccountEmailValidationDTO.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_AccountEmailValidationDTO _$$_AccountEmailValidationDTOFromJson(
+        Map<String, dynamic> json) =>
+    _$_AccountEmailValidationDTO(
+      result: json['result'] as String,
+    );
+
+Map<String, dynamic> _$$_AccountEmailValidationDTOToJson(
+        _$_AccountEmailValidationDTO instance) =>
+    <String, dynamic>{
+      'result': instance.result,
+    };
